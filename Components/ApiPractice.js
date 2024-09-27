f09964a28b962d1dfea1cf8eac0cbf38bc826a57
Your Name <lokeshtiwari212@gmail.com>
2024-09-27 11:07:26 +0530

    now my api is working fine and i handled all the errors

--- Components/ApiPractice.js
+++ Components/ApiPractice.js
@@ -1,29 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
 
 const ApiPractice = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error,setError] = useState(null);
 
-    useEffect(() => {
-        getProducts();
-    }, []);
-  
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   const getProducts = () => {
-    const URL = 'https://fakestoreapi.com/products/1';
+    const URL = 'https://fakestoreapi.com/products/';
 
-    fetch(URL).then(res => {
-       return res.json(); //convert it into readable format
-    }).then(data => {
+    fetch(URL)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        return res.json();
+      })
+      .then(data => {
+        setProducts(data);
+        setIsLoading(false);
         console.log(data);
-    })
-  }
+      })
+      .catch(error => {
+        setError(error.message);
+        console.log(error);
+      });
+  };
+
   return (
     <View>
-      <Text>ApiPractice</Text>
+      {isLoading ? (
+        <ActivityIndicator color="red" size="large" />
+      ) : error ? ( <Text>{error}</Text> ) : (
+        <FlatList
+          data={products}
+          renderItem={({item}) => (
+            <View>
+              <Image source={{uri: item.image}} style={styles.image} />
+              <Text>{item.price}</Text>
+            </View>
+          )}
+        />
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default ApiPractice
+export default ApiPractice;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 200,
+  },
+});
